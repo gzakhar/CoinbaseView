9a685398140768ce19c17e7705ab9e595459a72e
george <george.zakharov@libra.tech>
2019-08-01 11:39:24 -0400

    fix up recents

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,11 +1,8 @@
 package test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import domain.RecentRow;
-
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 
@@ -14,62 +11,18 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
 
-        FileReader fileReader = new FileReader("/Users/george/Downloads/test.json");
 
-        ObjectMapper mapper = new ObjectMapper();
-
-        RecentRow[] recent = mapper.readValue(fileReader, RecentRow[].class);
-
-
-//        Map<String, Map<Integer, List<RecentRow>>> res =  Arrays.stream(recent)
-//                .collect(Collectors.groupingBy(RecentRow::getExchange, Collectors.groupingBy(RecentRow::getTotalSeconds)));
-//
-//        System.out.println(res.get("Coinbase_pro").get(56321));
-
-
-        Arrays.stream(recent)
-                .collect(Collectors.groupingBy(RecentRow::getExchange, Collectors.groupingBy(RecentRow::getTotalSeconds)))
+        String[] names = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Z"};
+        final AtomicInteger countSeconds = new AtomicInteger();
+        final AtomicInteger countMints = new AtomicInteger();
+        Arrays.stream(names)
+                .collect(Collectors.groupingBy(i -> countSeconds.getAndIncrement() / 3))
                 .values()
-                .forEach(exchage -> {
-                    exchage.values()
-                            .stream()
-                            .map(second -> new SecondReturn(second.get(0).getExchange(), second.get(0).getPrice(), second.get(second.size() - 1).getPrice(), second.get(0).getTotalSeconds()));
-                });
+                .stream()
+                .map(v -> new Entry(v.get(0), v.get(2)))
+                .collect(Collectors.groupingBy(i -> countMints.getAndIncrement() / 5));
 
 
-//        Arrays.stream(recent)
-//                .collect(Collectors.groupingBy(RecentRow::getExchange, Collectors.groupingBy(RecentRow::getTotalSeconds)))
-//                .values()
-//                .forEach(exchage -> {
-//                    exchage.values()
-//                            .stream()
-//                            .map(second -> new SecondReturn(second.get(0).getExchange(), second.get(0).getPrice(), second.get(second.size() - 1).getPrice(), second.get(0).getTotalSeconds()))
-//                            .forEach();
-//                });
-
-
-//        Arrays.stream(recent)
-//                .collect(Collectors.groupingBy(RecentRow::getExchange, Collectors.groupingBy(RecentRow::getTotalSeconds)))
-//                .values()
-//                .forEach(System.out::println);
-//                .stream()
-//                .map(v -> new SecondReturn(v.get(0).getPrice(), v.get(v.size() - 1).getPrice(), v.get(0).getTotalSeconds()))
-//                .forEach(System.out::println);
-
-
-//
-//        String[] names = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Z"};
-//        final AtomicInteger countSeconds = new AtomicInteger();
-//        final AtomicInteger countMints = new AtomicInteger();
-//        Arrays.stream(names)
-//                .collect(Collectors.groupingBy(i -> countSeconds.getAndIncrement() / 3))
-//                .values()
-//                .stream()
-//                .map(v -> new Entry(v.get(0), v.get(2)))
-//                .collect(Collectors.groupingBy(i -> countMints.getAndIncrement() / 5))
-//                .values()
-//                .stream()
-//                .map();
 
 
     }
