2f9dc91116024d4fe1dc730029317b48b74ee00c
george <george.zakharov@libra.tech>
2019-08-01 11:43:02 -0400

    fixup with start

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -11,7 +11,6 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
 
-
         String[] names = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Z"};
         final AtomicInteger countSeconds = new AtomicInteger();
         final AtomicInteger countMints = new AtomicInteger();
@@ -23,169 +22,40 @@ public class Test {
                 .collect(Collectors.groupingBy(i -> countMints.getAndIncrement() / 5));
 
 
-
-
     }
 
 }
 
 class Entry {
-    private String open;
-    private String close;
+    private String start;
+    private String end;
 
-    public Entry(String open, String close) {
-        this.open = open;
-        this.close = close;
+    public Entry(String start, String end) {
+        this.start = start;
+        this.end = end;
     }
 
-    public String getOpen() {
-        return open;
+    public String getStart() {
+        return start;
     }
 
-    public void setOpen(String open) {
-        this.open = open;
+    public void setStart(String start) {
+        this.start = start;
     }
 
     public String getClose() {
-        return close;
+        return end;
     }
 
-    public void setClose(String close) {
-        this.close = close;
+    public void setClose(String end) {
+        this.end = end;
     }
 
     @Override
     public String toString() {
         return "Entry is{" +
-                "open='" + open + '\'' +
-                ", close='" + close + '\'' +
-                '}';
-    }
-}
-
-class FinEntry {
-    private String first;
-    private String last;
-
-    public FinEntry(String first, String last) {
-        this.first = first;
-        this.last = last;
-    }
-
-    public String getFirst() {
-        return first;
-    }
-
-    public void setFirst(String first) {
-        this.first = first;
-    }
-
-    public String getLast() {
-        return last;
-    }
-
-    public void setLast(String last) {
-        this.last = last;
-    }
-
-    @Override
-    public String toString() {
-        return "FinEntry is{" +
-                "first='" + first + '\'' +
-                ", last='" + last + '\'' +
-                '}';
-    }
-}
-
-
-class SecondReturn {
-    private String exchange;
-    private Double open;
-    private Double close;
-    private Integer time;
-
-    public SecondReturn(String exchange, Double open, Double close, Integer time) {
-        this.exchange = exchange;
-        this.open = open;
-        this.close = close;
-        this.time = time;
-    }
-
-
-    public String getExchange() {
-        return exchange;
-    }
-
-    public void setExchange(String exchange) {
-        this.exchange = exchange;
-    }
-
-    public Integer getTime() {
-        return time;
-    }
-
-    public void setTime(Integer time) {
-        this.time = time;
-    }
-
-    public Double getOpen() {
-        return open;
-    }
-
-    public void setOpen(Double open) {
-        this.open = open;
-    }
-
-    public Double getClose() {
-        return close;
-    }
-
-    public void setClose(Double close) {
-        this.close = close;
-    }
-
-
-    @Override
-    public String toString() {
-        return "SecondReturn{" +
-                "exchange='" + exchange + '\'' +
-                ", open=" + open +
-                ", close=" + close +
-                ", time=" + time +
-                '}';
-    }
-}
-
-class MinuteBin {
-    private Double open;
-    private Double close;
-
-    public MinuteBin(Double open, Double close) {
-        this.open = open;
-        this.close = close;
-    }
-
-    public Double getOpen() {
-        return open;
-    }
-
-    public void setOpen(Double open) {
-        this.open = open;
-    }
-
-    public Double getClose() {
-        return close;
-    }
-
-    public void setClose(Double close) {
-        this.close = close;
-    }
-
-    @Override
-    public String toString() {
-        return "MinuteBin{" +
-                "open=" + open +
-                ", close=" + close +
+                "start='" + start + '\'' +
+                ", end='" + end + '\'' +
                 '}';
     }
 }
